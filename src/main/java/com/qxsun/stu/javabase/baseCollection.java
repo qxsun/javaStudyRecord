227a1bbeb949352c4fa308e7d5f5da759c0f83e3
qxsun1@chinaums.com <qxsun1@chinaums.com>
2020-12-30 10:39:46 +0800

    添加代码和记录文档

---
+++ src/main/java/com/qxsun/stu/javabase/baseCollection.java
@@ -0,0 +1,78 @@
+package com.qxsun.stu.javabase;
+
+import java.util.ArrayDeque;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.TreeSet;
+
+/**
+ * qxsun学习工程：java基础之基础集合框架
+ * 学习教程：java8编程参考官方教程（第九版）-18章
+ * 2020年12月9日
+ * （1）
+ * (2) 18.9 使用映射
+ *
+ */
+public class baseCollection {
+    public static void main(String args[]) {
+        int exampleNum = 4;
+        //HashSet学习，默认容量是16，默认填充率是0.75，HashSet不能有序存储，如果需要有序，使用TreeSet
+        //LinkedHashSet 扩展了 HashSet，使其按照插入顺序遍历
+        if (2 == exampleNum) {
+            //构建一个HashSet对象
+           // HashSet<String> hs = new HashSet<>();
+            LinkedHashSet<String> hs = new LinkedHashSet<>();
+            //添加元素
+            hs.add("Beta");
+            hs.add("Alpha");
+            hs.add("Eta");
+            hs.add("Gama");
+            hs.add("Epsilon");
+            hs.add("Omega");
+            System.out.println(hs);
+        }
+        if (3 == exampleNum) {
+            //构建一个TreeSet对象
+            TreeSet<String> ts = new TreeSet<>();
+
+            //添加元素
+            ts.add("Beta");
+            ts.add("Alpha");
+            ts.add("Eta");
+            ts.add("Gama");
+            ts.add("Epsilon");
+            ts.add("Omega");
+            System.out.println(ts);
+        }
+
+        if (4 == exampleNum) {
+            //构建一个ArrayDeque对象
+            ArrayDeque<String> adq = new ArrayDeque<>();
+
+            //添加元素,这样可以实现一个栈的功能，后进先出。
+            adq.push("Beta");
+            adq.push("Alpha");
+            adq.push("Eta");
+            adq.push("Gama");
+            adq.push("Epsilon");
+            adq.push("Omega");
+            while(adq.peek() != null)
+                System.out.println(adq.pop() + " ");
+        }
+
+        //映射相关知识学习map
+
+        //学习比较器相关知识 18.10
+        if (6 == exampleNum) {
+
+        }
+
+        //18.11
+        //18.12 Arrays类
+
+
+
+
+
+    }
+}
---
+++ src/main/java/com/qxsun/stu/javabase/basePackage.java
@@ -0,0 +1,52 @@
+package com.qxsun.stu.javabase;
+
+/**
+ * qxsun学习工程：java基础之基础类库
+ * 学习教程：
+ * 2020年11月12日
+ * （1）学习String类
+ *(2)关于字符串比较 equals
+ *
+ */
+public class basePackage {
+    public static void main (String args[]){
+        int exampleNum = 2;
+        //字符串String初始化
+        if(0 == exampleNum)
+        {
+            String s = new String();
+            byte ascii[] = {65, 66, 67, 68, 69, 70};
+            String s1 = new String(ascii);
+            System.out.println(s1);
+            String s2 = new String(ascii, 2, 4);
+            System.out.println(s2);
+
+            int intPoints[] = {31, 32, 33, 34, 35, 36};
+            String s3 = new String(intPoints, 2, 4);
+            System.out.println(s3);
+        }
+
+        //equals方法
+        if(1 == exampleNum) {
+            String s01 = "Hello";
+            String s02 = "Hello";
+            String s03 = "Hello World";
+            String s04 = "HELLO";
+            System.out.println(s01 + " equals() " + s02 + "->" + s01.equals(s02));
+            System.out.println(s01 + " equals() " + s03 + "->" + s01.equals(s03));
+            System.out.println(s01 + " equals() " + s04 + "->" + s01.equals(s04));
+            System.out.println(s01 + " equalsIgnoreCase() " + s04 + "->" + s01.equalsIgnoreCase(s04));
+        }
+
+        //equals()方法比较String对象中的字符，符号“==”运算符比较两个对象的引用
+        if(2 == exampleNum) {
+            String s21 = "hello";
+            String s22 = new String(s21);
+            System.out.println(s21 + " equals() " + s22 + "->" + s21.equals(s22));
+            System.out.println(s21 + " == " + s22 + "->" + (s21==s22) );
+        }
+
+    }
+
+}
+
